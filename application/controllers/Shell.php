b5926d23e81acd4fb771fb8985d720e877ec10a5
ibugsec <ibugsec@localhost.localdomain>
2020-01-07 10:06:56 -0300

    inicio bucket amazon s3

--- application/controllers/Shell.php
+++ application/controllers/Shell.php
@@ -1,26 +1,15 @@
 <?php
-
 class Shell extends CI_Controller{
 
     public function __construct(){
         parent::__construct();
-        $this->servidor_imagens();
     }
     public function index(){
 
     }
-    /**
-     * Monta o servidor
-    **/
-    public function servidor_imagens(){
-        shell_exec('mkdir ../imagens');
-        shell_exec('chmod -R 777 ../imagens');
-        $this->response("success","Imagens ok");
+    public function php(){
 
-    }
-    public function rm_rf(){
-//        rm -rf
-
+        shell_exec('sudo apt-get install php-xml');
     }
 
 
