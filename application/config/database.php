71b072a8c79dd710c59c6b82c2b3e063d5f253b7
ibugsec <ibugsec@localhost.localdomain>
2020-02-24 10:24:35 -0300

    save

--- application/config/database.php
+++ application/config/database.php
@@ -6,7 +6,7 @@ $query_builder  = TRUE;
 
 $db['default']  = [
 	'dsn'	        => '',
-	'hostname'      => '54.167.207.17',
+	'hostname'      => 'localhost',
 	'username'      => 'postgres',
 	'password'      => 'k2l9g3v1',
 	'database'      => 'postgres',
