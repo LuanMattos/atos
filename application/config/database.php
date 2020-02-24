ce465adaecdee532c994289c55a3b35d9dde0400
ibugsec <ibugsec@localhost.localdomain>
2020-02-24 16:34:06 -0300

    erro no insert do cimongo

--- application/config/database.php
+++ application/config/database.php
@@ -27,7 +27,7 @@ $db['default']  = [
 ];
 
 $config['mongodb'] = [
-    'hostname'      => 'localhost',
+    'hostname'      => '127.0.0.1',
     'port'          => '27017',
     'username'      => 'atos',
     'password'      => '',
