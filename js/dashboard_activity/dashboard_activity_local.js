5a06d0f7c853da71b2e5834cc3260c2f3489a952
ibugsec <ibugsec@localhost.localdomain>
2020-03-08 16:43:44 -0300

    save

--- js/dashboard_activity/dashboard_activity_local.js
+++ js/dashboard_activity/dashboard_activity_local.js
@@ -126,7 +126,7 @@ var vue_instance_chat = new Vue({
 
             // Conectando
             // wss://echo.websocket.org
-            self.ws = new WebSocket('ws://127.0.0.11/:8050');
+            self.ws = new WebSocket('ws://atos.click/:8050');
 
             // Evento que será chamado ao abrir conexão
             self.ws.onopen = function(e) {
