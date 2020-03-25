ea5b38f78921529c30eed41feb90746853bc7462
ibugsec <ibugsec@localhost.localdomain>
2020-03-25 14:23:20 -0300

    save

--- socket/msg_socket/Chat.php
+++ socket/msg_socket/Chat.php
@@ -1,5 +1,6 @@
 <?php
 namespace Chat;
+use Mongo\MongoDb\MongoDb;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
@@ -13,7 +14,7 @@ class Chat implements MessageComponentInterface {
         $this->clients = new \SplObjectStorage;
         $this->subscriptions = [];
         $this->users = [];
-        new MongoDb\MongoDb();
+        new MongoDb();
     }
 
     public function onOpen( ConnectionInterface $conn ) {
