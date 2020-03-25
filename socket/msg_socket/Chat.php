215950cec978585088cb807940df568b80ac5acf
ibugsec <ibugsec@localhost.localdomain>
2020-03-24 21:47:37 -0300

    save

--- socket/msg_socket/Chat.php
+++ socket/msg_socket/Chat.php
@@ -54,7 +54,7 @@ class Chat implements MessageComponentInterface {
 
 //                        && $id != $from->resourceId
                         if ($channel == $target) {
-                            $this->users[$id]->send($data);
+                            $this->users[$id]->send($msg);
                         }
                     }
                 }
