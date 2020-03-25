6211f3859b0bf4346fb2270a896e0ed26a2f4b12
ibugsec <ibugsec@localhost.localdomain>
2020-03-25 13:28:02 -0300

    save

--- application/services/mongo/MongoDb.php
+++ application/services/mongo/MongoDb.php
@@ -1,12 +1,14 @@
 <?php
 namespace Service\Modules\Mongo;
 
-class MongoDb extends \CI_Model
+class MongoDb extends \SI_Controller
 {
     public $mongodb;
     public $mongomanager;
     public $mongobulkwrite;
     public function __construct(){
+        parent::__construct();
+
         $this->load_helpers();
         $this->conect_mongodb();
     }
