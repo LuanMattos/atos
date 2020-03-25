a2c634a8b0eb1b27f3baff13ccb090c80bda59b8
ibugsec <ibugsec@localhost.localdomain>
2020-03-25 13:59:06 -0300

    save

--- application/services/mongo/MongoDb.php
+++ application/services/mongo/MongoDb.php
@@ -4,9 +4,9 @@ use Service\GeneralService;
 
 class MongoDb extends GeneralService
 {
-    public $mongodb;
-    public $mongomanager;
-    public $mongobulkwrite;
+//    public $mongodb;
+//    public $mongomanager;
+//    public $mongobulkwrite;
 
 //        $this->load_helpers();
 //        $this->conect_mongodb();
