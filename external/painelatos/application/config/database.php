946b590cc48b944257dd642e84223e7c12600087
ibugsec <ibugsec@localhost.localdomain>
2020-05-17 13:08:46 -0300

    painel database

--- external/painelatos/application/config/database.php
+++ external/painelatos/application/config/database.php
@@ -1,29 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$active_group   = 'default';
+$query_builder  = TRUE;
 
-$active_group = 'default';
-$query_builder = TRUE;
+if(ENVIRONMENT === 'production'){
+    $hostname = 'www.atos.click';
+}else{
+    $hostname = 'localhost';
+}
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '52.204.82.217',
-	'port'     => '5400',
-	'username' => 'painel',
-	'password' => 'dfsaf435fd4gs46565674dfgs4fs654ts562hg5s4fga',
-	'database' => 'painel',
-	'dbdriver' => 'postgre',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
+    'dsn'	=> '',
+    'hostname' => $hostname,
+    'port'     => '5432',
+    'username' => 'painel',
+    'password' => 'eFdarksadfw4r54af4fd4a54h2fasfdg',
+    'database' => 'atos',
+    'dbdriver' => 'postgre',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
 //	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
