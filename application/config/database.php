ffbc4da2019c1810e04a678c7586dd43de872adf
ibugsec <ibugsec@localhost.localdomain>
2020-06-14 22:13:29 -0300

    convite ok spinner ok

--- application/config/database.php
+++ application/config/database.php
@@ -10,7 +10,7 @@ if(ENVIRONMENT === 'production'){
     $hostname = '54.237.142.136';
 }
 $db['default']  = [
-	'dsn'	        => '',
+	'dsn'	        => 'postgres',
 	'hostname'      => $hostname,
 	'username'      => 'postgres',
 	'port'          => '5432',
