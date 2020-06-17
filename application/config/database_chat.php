df4dbf9bdd5baea33d2f64acf1b609e69f5e2b92
luanmattos <patrickluan.matos@gmail.com>
2020-06-17 19:47:22 -0300

    sarvando

--- application/config/database_chat.php
+++ application/config/database_chat.php
@@ -20,7 +20,7 @@ class database_chat  {
 //        172.18.0.2
 
         return  [
-            'hostname'      => '172.18.0.2',
+            'hostname'      => '54.237.142.136',
             'port'          => '27017',
             'username'      => 'atos',
             'password'      => 'atos',
