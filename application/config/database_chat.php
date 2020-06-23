2d0554713a0ca516792f3f8df9ccaa8498f28217
luanmattos <patrickluan.matos@gmail.com>
2020-06-22 23:01:27 -0300

    database

--- application/config/database_chat.php
+++ application/config/database_chat.php
@@ -3,13 +3,13 @@ class database_chat  {
 
 
     public function config_mongo($shell = false){
-        $hostname = "172.18.0.2";
+        $hostname = "172.20.0.3";
 
         if($shell){
             $hostname = "localhost";
         }else{
             if(ENVIRONMENT === 'production'){
-                $hostname = '172.18.0.3';
+                $hostname = '172.20.0.3';
             }else{
                 $hostname = 'mongo';
             }
